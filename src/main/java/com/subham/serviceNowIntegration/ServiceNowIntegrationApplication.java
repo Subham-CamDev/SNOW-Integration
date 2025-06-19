af8a0e76edc29b0325df34f9e5f44e980640ff7c
Subham-CamDev <subhambhatta2212@gmail.com>
2025-06-19 09:43:30 +0530

    Added Better Logging

--- src/main/java/com/subham/serviceNowIntegration/ServiceNowIntegrationApplication.java
+++ src/main/java/com/subham/serviceNowIntegration/ServiceNowIntegrationApplication.java
@@ -3,8 +3,10 @@ package com.subham.serviceNowIntegration;
 import com.subham.serviceNowIntegration.configuration.ServiceNowProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -20,4 +22,10 @@ public class ServiceNowIntegrationApplication {
 	{
 		return new RestTemplate();
 	}
+
+	@Bean
+	public HttpMessageConverters customConverters() {
+		return new HttpMessageConverters(new MappingJackson2HttpMessageConverter());
+	}
+
 }

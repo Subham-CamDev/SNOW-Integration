b164544903a61751cbf4918c261487b90ed2d087
Subham-CamDev <subhambhatta2212@gmail.com>
2025-06-19 09:42:00 +0530

    Updated Controller

--- src/main/java/com/subham/serviceNowIntegration/controller/ServiceNowController.java
+++ src/main/java/com/subham/serviceNowIntegration/controller/ServiceNowController.java
@@ -1,29 +1,57 @@
 package com.subham.serviceNowIntegration.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.subham.serviceNowIntegration.model.IncidentDetailsDTO;
+import com.subham.serviceNowIntegration.model.RequestDetailsDTO;
+import com.subham.serviceNowIntegration.service.CatalogItemService;
 import com.subham.serviceNowIntegration.service.ServiceNowService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/snow")
+@Slf4j
 public class ServiceNowController {
 
-private final ServiceNowService service;
+    private final ServiceNowService service;
+    private final CatalogItemService itemService;
 
-public ServiceNowController(ServiceNowService service)
-{
-    this.service = service;
-}
+    public ServiceNowController(ServiceNowService service, CatalogItemService itemService)
+    {
+        this.service = service;
+        this.itemService = itemService;
+    }
 
-@PostMapping("/incident")
-public ResponseEntity<?> createSnowIncident(@RequestBody IncidentDetailsDTO incident)
-{
-    service.createIncident(incident);
-    return new ResponseEntity<>("Incident Created Successfully", HttpStatus.CREATED);
-}
+    @PostMapping(value = "/incident", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<String> createSnowIncident(@RequestPart("incident") @Validated String incidentJson,
+                                                     @RequestPart("attachment") MultipartFile file) throws JsonProcessingException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        IncidentDetailsDTO incident = mapper.readValue(incidentJson, IncidentDetailsDTO.class);
+
+        String ID = service.createIncident(incident, file);
+        if (ID != null) {
+            return new ResponseEntity<>("Incident " + ID + " Created Successfully", HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>("Incident creation failed!!", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/request")
+    public ResponseEntity<String> createSnowRequest(@RequestBody RequestDetailsDTO request)
+    {
+        String ID = service.createRequest(request);
+        if (ID != null) {
+            return new ResponseEntity<>("Request " + ID + " Created Successfully", HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>("Request creation failed!!", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
